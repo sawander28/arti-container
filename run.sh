c0d5c9775e6db26aaad85ee4244958ffbc6a600a
Sascha Wander wandsas <sascha.wander@onionmail.com>
2025-08-13 18:08:25 +0200

    arti

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 podman run \
     --rm \
     --name arti-container \
+    --network podman \
     -p 9050:9050 \
     -p 1053:1053 \
     -t arti-container
---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+podman logs arti-container
+podman container ps -a
+
+sudo ss -lnp | grep 9050
+sudo ss -lnp | grep 1053
+
+curl --proxy socks5h://127.0.0.1:9050 https://check.torproject.org/api/ip

b635d382ed202124cea05067c1c130b88b9b9cd3
Sascha Wander wandsas <sascha.wander@onionmail.com>
2025-08-19 02:26:17 +0200

    rmexamples

--- examples/checktor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl --proxy socks5h://127.0.0.1:9050 https://check.torproject.org/api/ip
--- examples/firewall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-iptables -t nat -A OUTPUT -m owner --uid-owner arti -j RETURN
-iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-port 9050
-
-iptables -t nat -A OUTPUT -p udp -dport 53 -j REDIRECT --to-port 1053
